876f8081a1312a5fbe7110098e49ae16a065c0a0
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2024-01-10 21:35:28 +0100

    initial commit

---
+++ 2. CARD_components/src/Card.jsx
@@ -0,0 +1,15 @@
+// import profilePic from "./assets/profile.png"
+
+function Card() {
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+            <h2 className="card-title">Hongmow</h2>
+            <p className="card-text">I am a software developer in development</p>
+        </div>
+    )
+    
+}
+
+
+export default Card
